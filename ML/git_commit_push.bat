b9c0d8905048250f2a56c48be7a737e2696b79cb
Serigne Bassirou NDIAYE <dstndiaye@gmail.com>
2024-07-20 16:49:52 +0200

    aaaaaa

---
+++ ML/git_commit_push.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Vérifier si un message de commit a été fourni
+if "%1"=="" (
+    echo Veuillez fournir un message de commit.
+    exit /b 1
+)
+
+REM Exécuter les commandes Git
+git add .
+git commit -m "%*"
+git push origin main
